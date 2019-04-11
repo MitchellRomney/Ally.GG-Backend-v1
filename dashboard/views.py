c91391ef4c06e89eede82c8b8b36f399bc010ed7
Mitchell Romney <mitchell@theearthissquare.com>
2019-04-11 17:10:10 +1000

    FIX: This is it boys.

--- dashboard/views.py
+++ dashboard/views.py
@@ -133,7 +133,7 @@ class MatchViewSet(viewsets.ModelViewSet):
         deleteAll = request.data['delete_all']
         if deleteAll == True:
             print('Deleting 1000 matches.')
-            Match.objects.all().values_list('pk')[:1000].delete()
+            Match.objects.filter(pk__in=Match.objects.all().values_list('pk')[:30000]).delete()
             print('1000 Matches Deleted')
             return Response('All Matches Deleted')
         existingMatch = Match.objects.filter(gameId=request.data['gameId'])
