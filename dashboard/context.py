e4e211366d9a68648fc59cdf93c382641967b203
Mitchell Romney <mitchell@theearthissquare.com>
2019-04-18 12:03:26 +1000

    SMALL: Number formatting to homepage stats.

--- dashboard/context.py
+++ dashboard/context.py
@@ -7,14 +7,14 @@ def global_context(request):
     isProfile = Profile.objects.all().filter(user=request.user) if request.user.is_authenticated == True else None
     profile = Profile.objects.get(user=request.user) if isProfile else None
     globalSettings = Setting.objects.get(name='Global') if Setting.objects.filter(name='Global').count() == 1 else Setting.objects.create(name='Global', latestVersion=get_latest_version())
+    total_kills = Player.objects.aggregate(Sum('kills'))
 
     return {
     'DEBUG': settings.DEBUG,
     'GSETTINGS': globalSettings,
-    'SUMMONER_COUNT': Summoner.objects.all().count(),
-    'TOTAL_KILLS': Player.objects.aggregate(Sum('kills')),
-    'TOTAL_DEATHS': Player.objects.aggregate(Sum('deaths')),
-    'MATCH_COUNT': Match.objects.all().count(),
+    'SUMMONER_COUNT': "{:,}".format(int(Summoner.objects.all().count()), 0),
+    'TOTAL_KILLS': "{:,}".format(int(total_kills['kills__sum']), 0),
+    'MATCH_COUNT': "{:,}".format(int(Match.objects.all().count()), 0),
     'MY_PROFILE': profile,
     'MY_SUMMONERS': Summoner.objects.filter(user_profile=profile) if isProfile else None,
     'CURRENT_PATCH': globalSettings.latestVersion,
