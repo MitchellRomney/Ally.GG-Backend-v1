1c398dd2f30d880ceb5dcc7ed74322ae915b8f62
Mitchell Romney <mitchell@theearthissquare.com>
2019-06-24 11:08:14 +1000

    Move login to custom method allowing case insensitive usernames

--- AllyGG/schema.py
+++ AllyGG/schema.py
@@ -1,6 +1,27 @@
 import graphene
 import graphql_jwt
 import dashboard.schema
+from django.contrib.auth import get_user_model
+from graphene_django import DjangoObjectType
+from graphql_jwt.utils import jwt_encode, jwt_payload
+from django.contrib.auth import authenticate, login
+
+
+class UserNode(DjangoObjectType):
+    token = graphene.String()
+
+    class Meta:
+        model = get_user_model()
+        filter_fields = [
+            'username',
+        ]
+
+    def resolve_token(self, info, **kwargs):
+        if info.context.user != self:
+            return None
+
+        payload = jwt_payload(self)
+        return jwt_encode(payload)
 
 
 class Query(dashboard.schema.Query, graphene.ObjectType):
@@ -9,6 +30,30 @@ class Query(dashboard.schema.Query, graphene.ObjectType):
 
 
 class Mutation(dashboard.schema.Mutation, graphene.ObjectType):
+
+    class Login(graphene.Mutation):
+        user = graphene.Field(UserNode)
+
+        class Arguments:
+            username = graphene.String()
+            password = graphene.String()
+
+        @classmethod
+        def mutate(cls, root, info, username, password):
+            user_model = get_user_model()
+
+            user_obj = user_model.objects.get(username__iexact=username)
+
+            user = authenticate(username=user_obj.username, password=password)
+
+            if user is None:
+                raise Exception('Please enter a correct username and password')
+
+            if not user.is_active:
+                raise Exception('It seems your account has been disabled')
+
+            login(info.context, user)
+            return cls(user=user)
 
     class ObtainJSONWebToken(graphql_jwt.JSONWebTokenMutation):
         user = graphene.Field(dashboard.schema.UserType)
@@ -22,6 +67,7 @@ class Mutation(dashboard.schema.Mutation, graphene.ObjectType):
     token_auth = ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
+    login = Login.Field()
     pass
 
 
