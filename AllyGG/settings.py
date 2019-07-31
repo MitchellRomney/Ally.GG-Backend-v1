dffe423ed27e9d336eebb3204a14f02576469630
Mitchell Romney <mitchell@theearthissquare.com>
2019-07-31 12:53:12 +1000

    Fix RabbitMQ channel layer

--- AllyGG/settings.py
+++ AllyGG/settings.py
@@ -185,7 +185,7 @@ ASGI_APPLICATION = 'AllyGG.routing.application'
 # Channels settings
 CHANNEL_LAYERS = {
    "default": {
-       "BACKEND": "channels_redis.core.RedisChannelLayer",
+       "BACKEND": "channels_rabbitmq.core.RabbitmqChannelLayer",
        "CONFIG": {
            "hosts": [os.environ.get('CLOUDAMQP_URL', 'redis://localhost:6379')],
        },
