e0a91f1d70e6c00f66957f26490efd7f8fc32b9c
Mitchell Romney <mitchell@theearthissquare.com>
2019-07-31 12:39:12 +1000

    Move channels to CloudAMQP

--- AllyGG/settings.py
+++ AllyGG/settings.py
@@ -187,7 +187,7 @@ CHANNEL_LAYERS = {
    "default": {
        "BACKEND": "channels_redis.core.RedisChannelLayer",
        "CONFIG": {
-           "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+           "hosts": [os.environ.get('CLOUDAMQP_URL', 'redis://localhost:6379')],
        },
    },
 }
