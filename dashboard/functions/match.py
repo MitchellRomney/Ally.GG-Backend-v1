5e9296831bf4c060e9d8a78a8565b1c3e21643c3
Mitchell Romney <mitchell@theearthissquare.com>
2019-07-31 12:55:59 +1000

    Fix bug caused by bots in game

--- dashboard/functions/match.py
+++ dashboard/functions/match.py
@@ -104,10 +104,6 @@ def create_match(game_id, server='OC1'):
         red_team = Team.objects.get(match=new_match, teamId=200)
         player_team = blue_team if player_account_info['participantId'] <= 5 else red_team
 
-        summoner_id = player_account_info['player']['summonerId']
-        summoner_server = player_account_info['player']['currentPlatformId']
-        summoner_name = player_account_info['player']['summonerName']
-
         for player_data in match_data['participants']:
             if player_data['participantId'] == player_account_info['participantId']:
 
@@ -116,6 +112,10 @@ def create_match(game_id, server='OC1'):
 
                 # Check if the player is a bot.
                 if player_account_info['player']['accountId'] != '0':
+
+                    summoner_id = player_account_info['player']['summonerId']
+                    summoner_server = player_account_info['player']['currentPlatformId']
+                    summoner_name = player_account_info['player']['summonerName']
 
                     # Add Summoner to database if it doesn't already exist.
                     if Summoner.objects.filter(summonerId=summoner_id, server=summoner_server).count() == 0:
